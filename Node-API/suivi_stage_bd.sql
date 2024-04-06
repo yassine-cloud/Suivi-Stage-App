3dee8942b1645863f7dc0047a8eb9124591a70e7
Yassine <saadaouiy51@gmail.com>
2024-04-06 06:14:01 +0100

    DEB

--- Node-API/suivi_stage_bd.sql
+++ Node-API/suivi_stage_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 05 avr. 2024 à 16:57
+-- Généré le : sam. 06 avr. 2024 à 07:11
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -50,26 +50,25 @@ INSERT INTO `condidature_stage` (`id_cs`, `id_etu`, `id_os`, `date`, `status`) V
 --
 
 CREATE TABLE `depot_stage` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
+  `id_ds` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `phone` varchar(50) NOT NULL,
-  `gender` varchar(50) NOT NULL,
-  `id_etudiant` int(11) NOT NULL,
-  `id_entreprise` int(11) NOT NULL
+  `tel` varchar(50) NOT NULL,
+  `genre` varchar(50) NOT NULL,
+  `id_etu` int(11) NOT NULL,
+  `id_os` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `depot_stage`
 --
 
-INSERT INTO `depot_stage` (`id`, `first_name`, `last_name`, `email`, `phone`, `gender`, `id_etudiant`, `id_entreprise`) VALUES
-(1, 'youssef', 'sighari', 'sighari@gmail.com', '23445555', 'male', 4, 2),
-(2, 'a', 'a', 'a', 'a', 'm', 4, 2),
-(3, 'xx', 'xx', 'xx', 'xx', 'm', 4, 1),
-(4, 'xxx', 'xx', 'xx', 'xx', 'm', 4, 1),
-(5, 'aa', 'aaa', 'a', 'aaaa', 'm', 4, 2);
+INSERT INTO `depot_stage` (`id_ds`, `nom`, `prenom`, `email`, `tel`, `genre`, `id_etu`, `id_os`, `date`, `status`) VALUES
+(1, 'youssef', 'sighari', 'sighari@gmail.com', '23445555', 'male', 4, 2, '2024-04-05', 'en attent'),
+(10, 'Soltani', 'Wissal', 'Wissa@gmail.com', '54983216', 'f', 2, 2, '2024-04-06', 'En attente');
 
 -- --------------------------------------------------------
 
@@ -108,16 +107,18 @@ CREATE TABLE `entreprise` (
   `password` varchar(255) NOT NULL,
   `secteuractivite` varchar(255) NOT NULL,
   `adresse` varchar(255) NOT NULL,
-  `contact` varchar(255) NOT NULL
+  `contact` varchar(255) NOT NULL,
+  `logo` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `entreprise`
 --
 
-INSERT INTO `entreprise` (`id_ent`, `nom`, `email`, `password`, `secteuractivite`, `adresse`, `contact`) VALUES
-(1, 'SAGEM', 'SAGEM@isetr.tn', '$2b$10$dHFdSUsjvLywnNr/8M9e8ueCmvOnL67D5dMEyg7hKdG50sScFsqHC', 'Informatique, Gestion', 'Tunis', '911'),
-(2, 'STAFIM', 'STAFIM@isetr.tn', '$2b$10$dHFdSUsjvLywnNr/8M9e8ueCmvOnL67D5dMEyg7hKdG50sScFsqHC', 'Informatique, Gestion', 'Tunis', '197');
+INSERT INTO `entreprise` (`id_ent`, `nom`, `email`, `password`, `secteuractivite`, `adresse`, `contact`, `logo`) VALUES
+(1, 'SAGEM', 'SAGEM@isetr.tn', '$2b$10$dHFdSUsjvLywnNr/8M9e8ueCmvOnL67D5dMEyg7hKdG50sScFsqHC', 'Informatique, Gestion, Réseau', 'Tunis', '9111', 'https://upload.wikimedia.org/wikipedia/fr/thumb/c/cc/LOGO_SAGEMCOM.png/800px-LOGO_SAGEMCOM.png?20200213095826'),
+(2, 'STAFIM', 'STAFIM@isetr.tn', '$2b$10$QBWhrbyz.s79K2MpiC3yfuocA/htu8xW.Ku64U1znmbe0i3IkIXIW', 'Informatique, Gestion, Voiture', 'Tunis', '19789', 'https://www.taa.tn/wp-content/uploads/2023/08/stafim-1.jpg'),
+(3, 'Rades', 'RAdes@isetr.tn', '$2b$10$KpmGB/DugYn0WDUDQjDpRuOHI4hrkaBIVCKTofHBb8ruu6QdI4ZUK', 'info, gestion', 'Rades', '54879632', 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Graduation_hat.svg');
 
 -- --------------------------------------------------------
 
@@ -143,7 +144,7 @@ INSERT INTO `etudiant` (`id_etu`, `nom`, `prenom`, `email`, `password`, `departe
 (1, 'Saadaoui', 'Yassine\r\n', 'yassinesaadaoui@isetr.tn', '$2b$10$dHFdSUsjvLywnNr/8M9e8ueCmvOnL67D5dMEyg7hKdG50sScFsqHC', 'Informatique', '78541236'),
 (2, 'Soltani', 'Wissal', 'wissalsoltani@isetr.tn', '$2b$10$dHFdSUsjvLywnNr/8M9e8ueCmvOnL67D5dMEyg7hKdG50sScFsqHC', 'Informatique', '98563214'),
 (3, 'Mechergui', 'Mohamed Aziz', 'azizmechergui@isetr.tn', '$2b$10$dHFdSUsjvLywnNr/8M9e8ueCmvOnL67D5dMEyg7hKdG50sScFsqHC', 'Informatique', '25983247'),
-(4, 'Sighari', 'Youssef', 'youssefsighari@isetr.tn', '$2b$10$dHFdSUsjvLywnNr/8M9e8ueCmvOnL67D5dMEyg7hKdG50sScFsqHC', 'Informatique', '55789632');
+(4, 'Sighari', 'Youssef', 'youssefsighari@isetr.tn', '$2b$10$dHFdSUsjvLywnNr/8M9e8ueCmvOnL67D5dMEyg7hKdG50sScFsqHC', 'Informatique', '55789632'),
 
 -- --------------------------------------------------------
 
@@ -167,7 +168,8 @@ CREATE TABLE `offre_stage` (
 
 INSERT INTO `offre_stage` (`id_os`, `titre`, `description`, `date_debut`, `date_fin`, `id_ent`, `nombre`) VALUES
 (1, 'Offre de stage dans STAFIM', 'description', '2024-01-11', '2024-02-03', 2, 10),
-(2, 'Offre de stage dans SAGEM', 'description2', '2024-01-11', '2024-02-03', 1, 3);
+(2, 'Offre de stage dans SAGEM', 'description2', '2024-01-11', '2024-02-03', 1, 3),
+(3, 'demande de Stagier pour STAFIM', 'Informatique', '2024-01-11', '2024-02-11', 2, 5);
 
 --
 -- Index pour les tables déchargées
@@ -185,9 +187,9 @@ ALTER TABLE `condidature_stage`
 -- Index pour la table `depot_stage`
 --
 ALTER TABLE `depot_stage`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_etudiant` (`id_etudiant`),
-  ADD KEY `id_entreprise` (`id_entreprise`);
+  ADD PRIMARY KEY (`id_ds`),
+  ADD KEY `id_etudiant` (`id_etu`),
+  ADD KEY `id_entreprise` (`id_os`);
 
 --
 -- Index pour la table `encadrant`
@@ -228,7 +230,7 @@ ALTER TABLE `condidature_stage`
 -- AUTO_INCREMENT pour la table `depot_stage`
 --
 ALTER TABLE `depot_stage`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_ds` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT pour la table `encadrant`
@@ -240,19 +242,19 @@ ALTER TABLE `encadrant`
 -- AUTO_INCREMENT pour la table `entreprise`
 --
 ALTER TABLE `entreprise`
-  MODIFY `id_ent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_ent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `etudiant`
 --
 ALTER TABLE `etudiant`
-  MODIFY `id_etu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_etu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `offre_stage`
 --
 ALTER TABLE `offre_stage`
-  MODIFY `id_os` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_os` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Contraintes pour les tables déchargées
@@ -269,7 +271,8 @@ ALTER TABLE `condidature_stage`
 -- Contraintes pour la table `depot_stage`
 --
 ALTER TABLE `depot_stage`
-  ADD CONSTRAINT `fg2` FOREIGN KEY (`id_entreprise`) REFERENCES `entreprise` (`id_ent`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `FK_ds_et` FOREIGN KEY (`id_etu`) REFERENCES `etudiant` (`id_etu`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_ds_os` FOREIGN KEY (`id_os`) REFERENCES `offre_stage` (`id_os`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `offre_stage`
